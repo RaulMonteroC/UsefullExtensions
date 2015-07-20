3d9c6be07c72b91f7fef61d213278de0fdc6bb40
Raul Montero <raulmonteroc01@gmail.com>
2015-07-20 14:35:01 -0400

    First project upload

---
+++ UsefullExtensions.Test/DatetimeExtensionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsefullExtensions;
+using NUnit.Framework;
+
+namespace UsefullExtensions.Test
+{
+    [TestFixture]
+    public class DatetimeExtensionTest
+    {
+        private DateTime date;
+
+        [SetUp]
+        public void SetUp()
+        {
+            date = new DateTime(2015, 7, 20);
+        }
+
+        [Test]
+        public void AddDaysWithNoWeekendBeetween()
+        {
+            Assert.AreEqual(date.AddWorkableDays(4), new DateTime(2015, 7, 24));
+        }
+
+        [Test]
+        public void AddDaysDoesntAddWeekends()
+        {
+            // When land in weekend
+            Assert.AreEqual(date.AddWorkableDays(5), new DateTime(2015, 7, 27));
+            // When passes weekend
+            Assert.AreEqual(date.AddWorkableDays(9), new DateTime(2015, 7, 31));
+            
+            Assert.AreEqual(date.AddWorkableDays(30), new DateTime(2015, 8, 31));
+        }
+    }
+}
---
+++ UsefullExtensions/DateTimeExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UsefullExtensions
+{
+    public static class DatetimeExtension
+    {
+        public static DateTime AddWorkableDays(this DateTime date, int days)
+        {
+            var newDate = date.AddDays(days);
+            var weekendDays = GetWeekendCount(date, newDate);
+            var holidays = GetHolidayCount(date, newDate);
+
+            newDate = newDate.AddDays(weekendDays);
+            newDate = newDate.AddDays(holidays);
+
+            return newDate;
+        }
+
+        private static int GetWeekendCount(DateTime date, DateTime finalDate)
+        {
+            var days = (finalDate - date).Days;
+            var weekendDays = (days / 7) * 2;
+
+            if (finalDate.DayOfWeek == DayOfWeek.Saturday || finalDate.DayOfWeek == DayOfWeek.Sunday)
+            {
+                weekendDays += 2;
+            }
+
+            return weekendDays;
+        }
+
+        private static int GetHolidayCount(DateTime originalDate, DateTime finalDate)
+        {
+            return 0;
+        }
+    }
+}
